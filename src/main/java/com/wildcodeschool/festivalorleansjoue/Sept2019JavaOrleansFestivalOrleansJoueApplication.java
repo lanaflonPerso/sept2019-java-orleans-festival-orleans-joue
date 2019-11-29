9433461a97f9604f314271b4f1671e9d88992e62
Ouchrif Ayoub <ouchrif.ayoub@gmail.com>
2019-11-29 21:50:08 +0100

    init project

---
+++ src/main/java/com/wildcodeschool/festivalorleansjoue/Sept2019JavaOrleansFestivalOrleansJoueApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.festivalorleansjoue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sept2019JavaOrleansFestivalOrleansJoueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sept2019JavaOrleansFestivalOrleansJoueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/festivalorleansjoue/Sept2019JavaOrleansFestivalOrleansJoueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.festivalorleansjoue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sept2019JavaOrleansFestivalOrleansJoueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
