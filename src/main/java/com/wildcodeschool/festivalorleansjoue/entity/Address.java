5b5770c45e40908af6d86d7bf98b52723d3c51fe
Sandra Roques <roquesandra@hotmail.fr>
2019-12-02 12:24:28 +0100

    add adress class + add OneToOne mapping between User and Adress

---
+++ src/main/java/com/wildcodeschool/festivalorleansjoue/entity/Address.java
@@ -0,0 +1,129 @@
+package com.wildcodeschool.festivalorleansjoue.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Address {
+	
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
+	private String wording;
+	private int phoneNumber1;
+	private int phoneNumber2;
+	private String remarks;
+	private String adress1;
+	private String adress2;
+	private String adress3;
+	private int postalCode;
+	private String city;
+	private String contactEmail;	
+	@OneToOne(mappedBy = "address")
+	private Address address;
+	
+	public Address() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getWording() {
+		return wording;
+	}
+
+	public void setWording(String wording) {
+		this.wording = wording;
+	}
+
+	public int getPhoneNumber1() {
+		return phoneNumber1;
+	}
+
+	public void setPhoneNumber1(int phoneNumber1) {
+		this.phoneNumber1 = phoneNumber1;
+	}
+
+	public int getPhoneNumber2() {
+		return phoneNumber2;
+	}
+
+	public void setPhoneNumber2(int phoneNumber2) {
+		this.phoneNumber2 = phoneNumber2;
+	}
+
+	public String getRemarks() {
+		return remarks;
+	}
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+
+	public String getAdress1() {
+		return adress1;
+	}
+
+	public void setAdress1(String adress1) {
+		this.adress1 = adress1;
+	}
+
+	public String getAdress2() {
+		return adress2;
+	}
+
+	public void setAdress2(String adress2) {
+		this.adress2 = adress2;
+	}
+
+	public String getAdress3() {
+		return adress3;
+	}
+
+	public void setAdress3(String adress3) {
+		this.adress3 = adress3;
+	}
+
+	public int getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(int postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getContactEmail() {
+		return contactEmail;
+	}
+
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}
