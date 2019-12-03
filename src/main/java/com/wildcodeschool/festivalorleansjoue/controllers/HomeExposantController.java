b9238d9ae589bc901ae2784dcc972c6041e23875
Sandra Roques <roquesandra@hotmail.fr>
2019-12-03 17:28:31 +0100

    send user to view

--- src/main/java/com/wildcodeschool/festivalorleansjoue/controllers/HomeExposantController.java
+++ src/main/java/com/wildcodeschool/festivalorleansjoue/controllers/HomeExposantController.java
@@ -3,6 +3,7 @@ package com.wildcodeschool.festivalorleansjoue.controllers;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,46 +12,58 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.wildcodeschool.festivalorleansjoue.entity.ErrorMsgModel;
 import com.wildcodeschool.festivalorleansjoue.entity.Event;
+import com.wildcodeschool.festivalorleansjoue.entity.User;
 import com.wildcodeschool.festivalorleansjoue.repository.EventRepository;
+import com.wildcodeschool.festivalorleansjoue.repository.UserRepository;
 
 @RestController
 public class HomeExposantController {	
 	
 	@Autowired 
 	EventRepository eventRepository;
+	@Autowired
+	UserRepository userRepository;
 	ErrorMsgModel noEvent;
 	ModelAndView model = new ModelAndView();
 	
 	@GetMapping("/accueil_editeur") 
 	public ModelAndView homeEditor() {
-		setEvent("homeEditor");
+		setModel("homeEditor");
 		return this.model;	
 	}
 
 	@GetMapping("/accueil_boutique") 
 	public ModelAndView homeshop() {
-		setEvent("homeShop");
+		setModel("homeShop");
 		return this.model;	
 	}
 	
 	@GetMapping("/accueil_volontaires") 
 	public ModelAndView homeVolunteers() {
-		setEvent("homeVolunteers");
+		setModel("homeVolunteers");
 		return this.model;	
 	}
 
-	public void setEvent (String route) {
+	public void setModel (String route) {
 		Date today = new Date();
-		List<Event> event = new ArrayList<>();
-		event = eventRepository.findByEventEndingDateAfter(today);
+		List<Event> events = new ArrayList<>();
+		events = eventRepository.findByEventEndingDateAfter(today);
 		model = new ModelAndView(route);
-		if (event.size() > 0) {
+		if (events.size() > 0) {
 			noEvent = new ErrorMsgModel();
-			model.addObject("event", event);
+			model.addObject("event", events);
 			model.addObject("eventError", noEvent);
 		} else {
 			noEvent = new ErrorMsgModel("Aucun évènnement en cours");
 			model.addObject("eventError", noEvent);
 		}
+		
+		User connectedUser= new User();
+		Optional<User> optionalUser = userRepository.findById(1L);		
+		if (optionalUser.isPresent()) {
+			connectedUser = optionalUser.get();
+        }
+		model.addObject("connectedUser", connectedUser);
+		
 	}
 }
