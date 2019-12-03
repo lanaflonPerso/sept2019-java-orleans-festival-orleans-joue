f05d9473d571ba434afed414550338d57d84df7a
52-28 <aymeric.feillault@gmail.com>
2019-12-03 14:11:52 +0100

    ajout du UserRole + modification data.sql

--- src/main/java/com/wildcodeschool/festivalorleansjoue/entity/UserRole.java
+++ src/main/java/com/wildcodeschool/festivalorleansjoue/entity/UserRole.java
@@ -1,5 +1,41 @@
 package com.wildcodeschool.festivalorleansjoue.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class UserRole {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String wording;
+	
+
+	public UserRole() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+	public String getWording() {
+		return wording;
+	}
+	
+	
+	public void setWording(String wording) {
+		this.wording = wording;
+	}
 }
