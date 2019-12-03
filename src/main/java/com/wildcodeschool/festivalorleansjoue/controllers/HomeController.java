d60e66080a4b5d4b84f77bedf8ae25df3aff33ca
Adrien MINICILLI <adrien.minicilli@gmail.com>
2019-12-03 13:26:40 +0100

    Added ErrorMsgModel and error msg added to view in accueil_exposants

--- src/main/java/com/wildcodeschool/festivalorleansjoue/controllers/HomeController.java
+++ src/main/java/com/wildcodeschool/festivalorleansjoue/controllers/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.wildcodeschool.festivalorleansjoue.entity.ErrorMsgModel;
 import com.wildcodeschool.festivalorleansjoue.entity.Event;
 import com.wildcodeschool.festivalorleansjoue.repository.EventRepository;
 
@@ -17,6 +18,7 @@ public class HomeController {
 	
 	@Autowired 
 	EventRepository eventRepository;
+	ErrorMsgModel noEvent;
 	
 	@GetMapping("/") 
 	public ModelAndView home(ModelAndView model) {
@@ -25,9 +27,13 @@ public class HomeController {
 		List<Event> event = new ArrayList<>();
 		event = eventRepository.findByEventEndingDateAfter(today);
 		model = new ModelAndView("home");
-		if(event != null) {
-			model.addObject(event);
+		if (event.size() > 0) {
+			model.addObject("event", event);	
+			return model;
+		} else {
+			noEvent = new ErrorMsgModel("Aucun évènnement en cours");
+			model.addObject("noEvent", noEvent);
+			return model;
 		}
-		return model;
 	}
 }
