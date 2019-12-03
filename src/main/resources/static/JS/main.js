30e8c9009c29fde543e23a89e895ef62d800f856
52-28 <aymeric.feillault@gmail.com>
2019-12-03 22:00:37 +0100

    nettoyage JS + responsive footer

--- src/main/resources/static/JS/main.js
+++ src/main/resources/static/JS/main.js
@@ -1,49 +1,17 @@
 jQuery(document).ready(function($){
-	/*******************
-		color swatch
-	********************/
-	//convert rgba color to hex color
-	$.cssHooks.backgroundColor = {
-	    get: function(elem) {
-	        if (elem.currentStyle)
-	            var bg = elem.currentStyle["background-color"];
-	        else if (window.getComputedStyle)
-	            var bg = document.defaultView.getComputedStyle(elem,
-	                null).getPropertyValue("background-color");
-	        if (bg.search("rgb") == -1)
-	            return bg;
-	        else {
-	            bg = bg.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-	            function hex(x) {
-	                return ("0" + parseInt(x).toString(16)).slice(-2);
-	            }
-	            return "#" + hex(bg[1]) + hex(bg[2]) + hex(bg[3]);
-	        }
-	    }
-	}
-	//set a label for each color swatch
-	$('.cd-color-swatch').each(function(){
-		var actual = $(this);
-		$('<b>'+actual.css("background-color")+'</b>').insertAfter(actual);
-	});
-
 
 	/*******************
 		main  navigation
 	********************/
 	var contentSections = $('main section');
-	//open navigation on mobile
-	$('.cd-nav-trigger').on('click', function(){
-		$('header').toggleClass('nav-is-visible');
-	});
+
 	//smooth scroll to the selected section
 	$('.cd-main-nav a[href^="#"]').on('click', function(event){
         event.preventDefault();
-        $('header').removeClass('nav-is-visible');
         var target= $(this.hash),
         	topMargin = target.css('marginTop').replace('px', ''),
-        	hedearHeight = $('header').height();
-        $('body,html').animate({'scrollTop': parseInt(target.offset().top - hedearHeight - topMargin)}, 200); 
+        	headerHeight = $('header').height();
+        $('body,html').animate({'scrollTop': parseInt(target.offset().top - headerHeight - topMargin)}, 200); 
     });
     //update selected navigation element
     $(window).on('scroll', function(){
