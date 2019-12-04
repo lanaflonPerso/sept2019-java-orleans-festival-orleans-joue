4b611d4c313a262e0e25a3d2a5ce30d139c1a56f
Sandra Roques <roquesandra@hotmail.fr>
2019-12-04 16:27:08 +0100

    event ---> events

--- src/main/java/com/wildcodeschool/festivalorleansjoue/services/ModelService.java
+++ src/main/java/com/wildcodeschool/festivalorleansjoue/services/ModelService.java
@@ -46,12 +46,12 @@ public class ModelService {
 	public void setHomeModel (String route) {
 		
 		Date today = new Date();
-		List<Event> event = new ArrayList<>();
-		event = eventRepository.findByEventEndingDateAfter(today);
+		List<Event> events = new ArrayList<>();
+		events = eventRepository.findByEventEndingDateAfter(today);
 		this.errorModel = new ErrorMsgModel();
 		this.model = new ModelAndView(route);
 		
-		if (event.size() == 0) {
+		if (events.size() == 0) {
 			this.errorModel.setErrorON(true);
 			this.errorModel.setErrorMsg("Aucun évènnement en cours.");
 		}
@@ -63,7 +63,7 @@ public class ModelService {
         }
 		
 		if (connectedUser.getUserRole().getWording().equals("editeur")) {
-			for (Event event2 : event) {
+			for (Event event2 : events) {
 				if (event2.getEditorsRegistrationBeginDate().before(today) && event2.getEditorsRegistrationEndDate().after(today)) {
 					event2.setRegistrationOpen(true);
 				}
@@ -72,7 +72,7 @@ public class ModelService {
 		}
 						
 		else if (connectedUser.getUserRole().getWording().equals("boutique")) {
-			for (Event event2 : event) {
+			for (Event event2 : events) {
 				if (event2.getShopsRegistrationBeginDate().before(today) && event2.getShopsRegistrationEndDate().after(today)) {
 					event2.setRegistrationOpen(true);
 				}
@@ -81,7 +81,7 @@ public class ModelService {
 		}
 		
 		else if (connectedUser.getUserRole().getWording().equals("benevole")) {
-			for (Event event2 : event) {
+			for (Event event2 : events) {
 				if (event2.getVolunteersRegistrationBeginDate().before(today) && event2.getVolunteersRegistrationEndDate().after(today)) {
 					event2.setRegistrationOpen(true);
 				}
@@ -91,7 +91,7 @@ public class ModelService {
 		
 		this.model.addObject("connectedUser", connectedUser);
 		this.model.addObject("eventError", this.errorModel);
-		this.model.addObject("event", event);		
+		this.model.addObject("event", events);		
 	}
 }
 
