0b7308457add63a695487319c84f3514b20e550c
Sandra Roques <roquesandra@hotmail.fr>
2019-12-09 10:01:06 +0100

    correction progressbar

--- src/main/resources/static/JS/subscribeEditor.js
+++ src/main/resources/static/JS/subscribeEditor.js
@@ -12,7 +12,8 @@ $(".next").click(function(){
 	next_fs = $(this).closest('form').next();
 	
 	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("form").index(next_fs)).addClass("active");
+	$("#progressbar_1 li").eq($("form").index(next_fs)).addClass("active");
+    $("#progressbar_2 li").eq($("form").index(next_fs)).addClass("active");
 	
 	//show the next fieldset
 	next_fs.show(); 
@@ -50,7 +51,8 @@ $(".previous").click(function(){
 	previous_fs = $(this).closest('form').prev();
 	
 	//de-activate current step on progressbar
-	$("#progressbar li").eq($("form").index(current_fs)).removeClass("active");
+	$("#progressbar_1 li").eq($("form").index(current_fs)).removeClass("active");
+    $("#progressbar_2 li").eq($("form").index(current_fs)).removeClass("active");
 	
 	//show the previous fieldset
 	previous_fs.show(); 
