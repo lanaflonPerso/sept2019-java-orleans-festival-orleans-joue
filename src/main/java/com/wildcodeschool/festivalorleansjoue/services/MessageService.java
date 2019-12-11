8ee1dc66195d19e09f5015b44adc5e3622a1923c
52-28 <aymeric.feillault@gmail.com>
2019-12-11 11:10:27 +0100

    modification saut de lignes

--- src/main/java/com/wildcodeschool/festivalorleansjoue/services/MessageService.java
+++ src/main/java/com/wildcodeschool/festivalorleansjoue/services/MessageService.java
@@ -1,7 +1,6 @@
 package com.wildcodeschool.festivalorleansjoue.services;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import com.wildcodeschool.festivalorleansjoue.entity.Event;
--- src/main/java/com/wildcodeschool/festivalorleansjoue/services/NavbarLinksService.java
+++ src/main/java/com/wildcodeschool/festivalorleansjoue/services/NavbarLinksService.java
@@ -2,6 +2,7 @@ package com.wildcodeschool.festivalorleansjoue.services;
 
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class NavbarLinksService {
 	
