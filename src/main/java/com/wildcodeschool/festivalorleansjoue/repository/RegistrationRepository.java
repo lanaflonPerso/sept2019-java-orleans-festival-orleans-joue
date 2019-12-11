20cf058931d9d806ba611c1c176dadbc7e8049eb
52-28 <aymeric.feillault@gmail.com>
2019-12-11 11:12:59 +0100

    modification saut de lignes + suppression query

--- src/main/java/com/wildcodeschool/festivalorleansjoue/repository/RegistrationRepository.java
+++ src/main/java/com/wildcodeschool/festivalorleansjoue/repository/RegistrationRepository.java
@@ -1,7 +1,6 @@
 package com.wildcodeschool.festivalorleansjoue.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,8 +10,4 @@ import com.wildcodeschool.festivalorleansjoue.entity.Registration;
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
 
 	public List<Registration> findByState(String state);
-	
-	@Query(value ="SELECT * from registration JOIN society_referent ON society_referent.id=registration.society_id "
-			+ "JOIN user ON user.id=society_referent.user_id WHERE user.id=?1", nativeQuery = true)
-	public List<Registration> findByUserId(Long userId);
 }
