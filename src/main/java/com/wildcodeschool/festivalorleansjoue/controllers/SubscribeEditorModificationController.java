e13b9d757ee5ac9c06531a53ac395082f9692cc6
52-28 <aymeric.feillault@gmail.com>
2019-12-11 16:19:42 +0100

    ajout template modification inscription

--- src/main/java/com/wildcodeschool/festivalorleansjoue/controllers/SubscribeEditorModificationController.java
+++ src/main/java/com/wildcodeschool/festivalorleansjoue/controllers/SubscribeEditorModificationController.java
@@ -4,19 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.wildcodeschool.festivalorleansjoue.services.ModelService;
 
+@RestController
 public class SubscribeEditorModificationController {
 	
 	@Autowired
 	ModelService modelService;
 	
 	@GetMapping("/subscribeEditorModification")
-	public ModelAndView subscribeEditor(@RequestParam int id) {
-			
-		modelService.setSubscribeEditorModel("subscribeEditor",id);
+	public ModelAndView subscribeEditor() {
+		//TODO a modifier! (id en deuxieme parametre)
+		modelService.setSubscribeEditorModel("subscribeEditorModification",1);
 		return modelService.getModel();
 	}
 }
