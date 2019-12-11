3122414d6b3e9e269b98ad44a247dfb5e24b3bee
52-28 <aymeric.feillault@gmail.com>
2019-12-11 10:32:05 +0100

    modification saut de lignes

--- src/main/java/com/wildcodeschool/festivalorleansjoue/entity/User.java
+++ src/main/java/com/wildcodeschool/festivalorleansjoue/entity/User.java
@@ -31,8 +31,6 @@ public class User {
 	private UserRole userRole;
  
 	
-	
-
 	public User() {
 
 	}
--- src/main/java/com/wildcodeschool/festivalorleansjoue/repository/GameRepository.java
+++ src/main/java/com/wildcodeschool/festivalorleansjoue/repository/GameRepository.java
@@ -2,7 +2,6 @@ package com.wildcodeschool.festivalorleansjoue.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.wildcodeschool.festivalorleansjoue.entity.Game;
 
 @Repository
