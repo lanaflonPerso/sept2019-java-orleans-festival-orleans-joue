9755baf73f26a51d69c661434352dd2a94347f29
52-28 <aymeric.feillault@gmail.com>
2019-12-11 10:17:49 +0100

    modification indentation

--- src/main/java/com/wildcodeschool/festivalorleansjoue/controllers/SubscribeEditorController.java
+++ src/main/java/com/wildcodeschool/festivalorleansjoue/controllers/SubscribeEditorController.java
@@ -30,12 +30,14 @@ import com.wildcodeschool.festivalorleansjoue.services.ModelService;
 		
 		@PostMapping("/subscribeEditor")
 		public ModelAndView subscribeEditor(@RequestParam int id) {
+			
 			modelService.setSubscribeEditorModel("subscribeEditor",id);
 			return modelService.getModel();
 		}
 		
 		@PostMapping("/submitRegistration")
 		public ModelAndView submitRegistration(@ModelAttribute Registration registration, @RequestParam String eventId) {
+			
 			Event e = eventRepository.getOne(Long.parseLong(eventId));
 			registration.setEvent(e);
 			if (registration.getTablesQuantity() > e.getMaxTablesPerEditor()) {
@@ -49,6 +51,7 @@ import com.wildcodeschool.festivalorleansjoue.services.ModelService;
 		
 		@GetMapping("/stopRegistration")
 		public ModelAndView stopRegistration() {
+			
 			ModelMap model = new ModelMap();
 			model.addAttribute("hasSubscribe", "ko");
 			return new ModelAndView("redirect:/accueil_editeur", model);
