b90734f912a153bd5f4aed64dc5d852934b4b950
Adrien MINICILLI <adrien.minicilli@gmail.com>
2019-12-12 14:42:30 +0100

    added reg repo method

--- src/main/java/com/wildcodeschool/festivalorleansjoue/repository/RegistrationRepository.java
+++ src/main/java/com/wildcodeschool/festivalorleansjoue/repository/RegistrationRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.wildcodeschool.festivalorleansjoue.entity.Registration;
+import com.wildcodeschool.festivalorleansjoue.entity.Society;
 
 @Repository
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
 
 	public List<Registration> findByState(String state);
+	public List<Registration> findBySociety(Society society); 
 }
