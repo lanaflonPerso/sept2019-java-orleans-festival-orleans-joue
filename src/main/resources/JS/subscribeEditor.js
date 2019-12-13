c7fb183575c12044e567b2bcbf6edff7db5aa952
Sandra Roques <roquesandra@hotmail.fr>
2019-12-13 10:35:22 +0100

    bouton next bloqué si condition non ok

--- src/main/resources/JS/subscribeEditor.js
+++ src/main/resources/JS/subscribeEditor.js
@@ -1,10 +1,46 @@
+//Registration cost preview and final view
+var table_quantity = 0;
+var sale_option_choice = 0;
+var price_per_table, sale_option_price, max_table_quantity;
+var registrationCost;
+
+price_per_table = parseFloat(document.getElementById("pricePerTable").value);
+sale_option_price = parseFloat(document.getElementById("saleOptionPrice").value);
+
+price_per_table = 65;
+sale_option_price = 100;
+max_table_quantity = 20;
+
+document.getElementById("saleOption").addEventListener("change", function (e) {
+    if (e.target.checked == false) {
+        sale_option_choice = 0;
+    } else {
+        sale_option_choice = sale_option_price;
+    }
+    registrationCost = table_quantity * price_per_table - parseInt(table_quantity / 4) * price_per_table + sale_option_choice;
+    document.getElementById("costPreview").textContent = "Prévisualisation du coût de l'inscription : " + registrationCost.toString() + " €";
+    document.getElementById("finalCost").textContent = "Coût de l'inscription : " + registrationCost.toString() + " €";
+});
+
+document.getElementById('tablesQuantity').addEventListener('input', function () {
+    table_quantity = parseInt(this.value);
+
+    if (table_quantity < 0 || table_quantity == NaN) {
+        table_quantity = 0;
+    }
+    registrationCost = table_quantity * price_per_table - parseInt(table_quantity / 4) * price_per_table + sale_option_choice;
+    document.getElementById("costPreview").textContent = "Prévisualisation du coût de l'inscription : " + registrationCost.toString() + " €";
+    document.getElementById("finalCost").textContent = "Coût de l'inscription : " + registrationCost.toString() + " €";
+
+});
+
 //Animations
 var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
 
 $(".next").click(function () {
-    if (animating) return false;
+    if (animating || table_quantity <= 0 || table_quantity > max_table_quantity) return false;
     animating = true;
 
     current_fs = $(this).parent();
@@ -94,6 +130,11 @@ $(".submit").click(function () {
     return false;
 });
 
+
+
+
+
+
 //Abandon confirm message
 function confirmation() {
     if (confirm("Êtes-vous sûr de vouloir annuler l'inscription ?")) {
@@ -102,45 +143,11 @@ function confirmation() {
 }
 
 //Don't submit with enter button
-$(document).ready(function() {
-  $(window).keydown(function(event){
-    if(event.keyCode == 13) {
-      event.preventDefault();
-      return false;
-    }
-  });
-});
-
-//Registration cost preview and final view
-var table_quantity = 0;
-var sale_option_choice = 0;
-var price_per_table, sale_option_price;
-var registrationCost;
-
-price_per_table = parseFloat(document.getElementById("pricePerTable").value);
-sale_option_price = parseFloat(document.getElementById("saleOptionPrice").value);
-
-document.getElementById("saleOption").addEventListener("change", function (e) {
-    if (e.target.checked == false) {
-        sale_option_choice = 0;
-    } else {
-        sale_option_choice = sale_option_price;
-    }
-    registrationCost = table_quantity * price_per_table - parseInt(table_quantity / 4) * price_per_table + sale_option_choice;
-    document.getElementById("costPreview").textContent = "Prévisualisation du coût de l'inscription : " + registrationCost.toString() + " €";
-    document.getElementById("finalCost").textContent = "Coût de l'inscription : " + registrationCost.toString() + " €";
-
-});
-
-document.getElementById('tablesQuantity').addEventListener('change', function () {
-    table_quantity = parseFloat(this.value);
-    if (table_quantity >= 20) {
-    	table_quantity = 20;
-    }
-    if (table_quantity <= 0) { 
-    	table_quantity = 0;
-    }
-    registrationCost = table_quantity * price_per_table - parseInt(table_quantity / 4) * price_per_table + sale_option_choice;
-    document.getElementById("costPreview").textContent = "Prévisualisation du coût de l'inscription : " + registrationCost.toString() + " €";
-    document.getElementById("finalCost").textContent = "Coût de l'inscription : " + registrationCost.toString() + " €";
+$(document).ready(function () {
+    $(window).keydown(function (event) {
+        if (event.keyCode == 13) {
+            event.preventDefault();
+            return false;
+        }
+    });
 });
