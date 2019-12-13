407cfe77008349265198a5a918b7ecfc68932e4b
Sandra Roques <roquesandra@hotmail.fr>
2019-12-13 14:03:31 +0100

    modif affichage home

--- src/main/java/com/wildcodeschool/festivalorleansjoue/entity/Event.java
+++ src/main/java/com/wildcodeschool/festivalorleansjoue/entity/Event.java
@@ -5,12 +5,14 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
+import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
@@ -23,6 +25,8 @@ public class Event {
 	private Long id;
 	private String name;
 	private short tables;
+	@Lob
+	@Column
 	private String description;
 	private byte protozoneTables;
 	private byte maxTablesPerEditor;
