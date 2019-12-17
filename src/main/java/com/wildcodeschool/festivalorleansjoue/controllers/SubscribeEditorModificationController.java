b35121b3790d1f4f783d5131c018438e50e414f9
Adrien MINICILLI <adrien.minicilli@gmail.com>
2019-12-17 14:24:39 +0100

    homepage hr fix

--- src/main/java/com/wildcodeschool/festivalorleansjoue/controllers/SubscribeEditorModificationController.java
+++ src/main/java/com/wildcodeschool/festivalorleansjoue/controllers/SubscribeEditorModificationController.java
@@ -1,15 +1,14 @@
 package com.wildcodeschool.festivalorleansjoue.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.wildcodeschool.festivalorleansjoue.services.ModelService;
 
-@RestController
+@Controller
 public class SubscribeEditorModificationController {
 
 	@Autowired
